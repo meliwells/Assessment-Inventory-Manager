7820252c733cfedc758d59c54dac96678e235d23
Melissa Wells <meliwells08@gmail.com>
2025-02-18 14:44:18 -0700

    update product progress

--- src/main/java/com/assessment/inventoryManager/InventoryManagerApplicationCLI.java
+++ src/main/java/com/assessment/inventoryManager/InventoryManagerApplicationCLI.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.Console;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -22,8 +23,11 @@ public class InventoryManagerApplicationCLI implements CommandLineRunner {
 
     public void run(String[] args) {
         //test add product
-        System.out.println("Add product");
-        addProduct();
+        //System.out.println("Add product");
+        //addProduct();
+
+        //test update product
+        updateProduct();
     }
 
     public void addProduct() {
@@ -64,6 +68,35 @@ public class InventoryManagerApplicationCLI implements CommandLineRunner {
     }
 
     public void updateProduct() {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Update a Product");
+        System.out.println("Enter the product Id: ");
+        int productId = console.nextInt();
+        console.nextLine();
+
+        System.out.println("enter the new quantity (or press enter to skip): ");
+        int quantity = console.nextInt();
+        console.nextLine();
+
+        System.out.println("Enter the new price (or press enter to skip): ");
+        double price = Double.parseDouble(console.nextLine());
+
+        while (true) {
+            Optional<Product> updateProduct = productRepository.findById(productId);
+            if (updateProduct.isPresent()) {
+                System.out.println("Product Id" + productId + " found");
+                Product product = updateProduct.get();
+                updateProduct.get().setQuantity(quantity);
+                updateProduct.get().setPrice(price);
+
+                productRepository.save(product);
+                break;
+            } else {
+                System.out.println("Product Id not found. Please enter a different Id.");
+            }
+        }
+
+
 
     }
 }
