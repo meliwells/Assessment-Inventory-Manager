7974c33f73ae189ac28488a59b18f359f83b2ebf
Melissa Wells <meliwells08@gmail.com>
2025-02-23 11:39:45 -0700

    working on main menu

--- src/main/java/com/assessment/inventoryManager/ui/Console.java
+++ src/main/java/com/assessment/inventoryManager/ui/Console.java
@@ -3,6 +3,15 @@ package com.assessment.inventoryManager.ui;
 import java.util.Scanner;
 
 public class Console {
+    Scanner console = new Scanner(System.in);
+    boolean isRunning;
 
+    public Console(Scanner console, boolean isRunning) {
+        this.console = console;
+        this.isRunning = isRunning;
     }
+
+
+}
+
 
