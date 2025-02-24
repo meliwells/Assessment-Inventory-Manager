4944eacb1bc47493fdf2e1251ff9ae7d5eba2f4a
Melissa Wells <meliwells08@gmail.com>
2025-02-23 17:26:37 -0700

    progress

--- src/main/java/com/assessment/inventoryManager/repository/ProductRepository.java
+++ src/main/java/com/assessment/inventoryManager/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.assessment.inventoryManager.repository;
 import com.assessment.inventoryManager.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    //Optional<Product> findById(int productId);
+    //Optional<Product> findByName(String productName);
 }
